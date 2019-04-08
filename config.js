64c25f5db6fe7e1a56dcff9e5b1168ce197bff16
Missy Maloney <missymaloney1@gmail.com>
2019-04-08 11:54:30 -0400

    changed server.js

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.MONGO_DATABASE_URL = process.env.MONGO_DATABASE_URL || "mongodb://localhost/efu-localdb";
+exports.PORT = process.env.PORT || '8080';
--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 'use strict'
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-const {PORT} = process.env.PORT || 3000;
+const {PORT, MONGO_DATABASE_URL} = require('./config');
 
 app.get('/api/*', (req, res) => {
     res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
 
-module.exports = {app};
+function startServer(databaseUrl, port=PORT) {
+    return new Promise((resolve,reject) => {
+            let mongoUrl;
+            mongoose.connect(databaseUrl, { useNewUrlParser: true }, err => { 
+            if(err) {
+                console.error(err);
+                return reject(err);
+            } else {
+
+                server = app.listen(port, () => {
+                                        console.log(`Express server listening on http://localhost:${port}`);
+                    resolve();
+                })
+                .on('error', err => {
+                                        mongoose.disconnect();
+                    console.error(err);
+                    reject(err);
+                });
+            }
+        });
+    });
+}
+
+function stopServer() {
+    return mongoose
+        .disconnect()
+        .then(() =>  new Promise((resolve, reject) => {
+            server.close(err => {
+                if(err) {
+                    console.error(err);
+                    return reject(err);
+                } else {
+                    console.log('Express server stopped.');
+                    resolve();
+                }
+            });
+        }));
+}
+
+if (require.main === module) { startServer(MONGO_DATABASE_URL).catch(err => console.error(err)); }
+
+module.exports = {app, startServer, stopServer};
