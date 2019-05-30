73d9b2c204fa82048ebdec830f75e4b97aa68a83
Missy Maloney <missymaloney1@gmail.com>
2019-05-30 07:04:41 -0400

    changed jwt expiry error

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 exports.MONGO_DATABASE_URL = process.env.MONGO_DATABASE_URL || "mongodb://localhost/efu-localdb";
-//exports.MONGO_DATABASE_URL = process.env.MONGO_DATABASE_URL || "mongodb://admin:Daisl9515@ds217360.mlab.com:17360/efu-db1";
 exports.PORT = process.env.PORT || '8080';
 exports.JWT_SECRET = process.env.JWT_SECRET || "Daisl";
-exports.JWT_EXPIRY = process.send.JWT_EXPIRY || '7d';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
